8fb23af1b486bfa562795994be667cd7a7117980
Alain Rodriguez <arodrime@gmail.com>
2026-09-26 08:29:33 +0200

    cassandra_repository/cassandra_install: ship the signing keys, write apt sources as deb822

--- roles/cassandra_repository/molecule/default/tests/test_default.py
+++ roles/cassandra_repository/molecule/default/tests/test_default.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 import testinfra.utils.ansible_runner
@@ -23,23 +24,29 @@ def get_cassandra_apt_keyring_path(host):
     return include_vars(host)['ansible_facts']['cassandra_apt_keyring_path']
 
 
+def is_redhat(host):
+    return host.file("/etc/redhat-release").exists \
+        or host.system_info.distribution in ("amzn", "redhat", "centos", "rocky", "almalinux")
+
+
+def is_debian(host):
+    return host.system_info.distribution in ("debian", "ubuntu")
+
+
 def test_redhat_cassandra_repository_file(host):
-    # with capsys.disabled(): #Disable autocapture of output and send to stdout N.B capsys must be passed into function
-    # print(include_vars(host)['ansible_facts'])
     cassandra_version = get_cassandra_version(host)
-    if host.system_info.distribution == "redhat" \
-            or host.system_info.distribution == "centos":
+    if is_redhat(host):
         f = host.file("/etc/yum.repos.d/cassandra-{0}.repo".format(cassandra_version))
         assert f.exists
         assert f.user == 'root'
         assert f.group == 'root'
         assert f.mode == 0o644
+        assert "gpgkey = file:///etc/pki/rpm-gpg/apache-cassandra.asc" in f.content_string
 
 
 def test_redhat_yum_search(host):
     cassandra_version = get_cassandra_version(host)
-    if host.system_info.distribution == "redhat" \
-            or host.system_info.distribution == "centos":
+    if is_redhat(host):
         cmd = host.run("yum search cassandra --disablerepo='*' \
                             --enablerepo='cassandra-{0}'".format(cassandra_version))
 
@@ -47,32 +54,35 @@ def test_redhat_yum_search(host):
         assert "cassandra" in cmd.stdout
 
 
+def test_signing_keys_shipped_with_the_role(host):
+    path = "/etc/pki/rpm-gpg/apache-cassandra.asc" if is_redhat(host) \
+        else get_cassandra_apt_keyring_path(host)
+    f = host.file(path)
+    assert f.exists
+    assert f.mode == 0o644
+    with open(os.path.join(os.path.dirname(__file__), "..", "..", "..", "files", "KEYS"), "rb") as shipped:
+        assert f.sha256sum == hashlib.sha256(shipped.read()).hexdigest()
+
+
 def test_debian_cassandra_repository_file(host):
     cassandra_version = get_cassandra_version(host)
     keyring_path = get_cassandra_apt_keyring_path(host)
-    if host.system_info.distribution == "debian" \
-            or host.system_info.distribution == "ubuntu":
-        f = host.file("/etc/apt/sources.list.d/cassandra-{0}.list".format(cassandra_version))
+    if is_debian(host):
+        assert not host.file("/etc/apt/sources.list.d/cassandra-{0}.list".format(cassandra_version)).exists
+        f = host.file("/etc/apt/sources.list.d/cassandra-{0}.sources".format(cassandra_version))
 
         assert f.exists
         assert f.user == 'root'
         assert f.group == 'root'
         assert f.mode == 0o644
-        assert f.content_string.strip() == "deb [signed-by={0}] https://debian.cassandra.apache.org {1} main".format(keyring_path, cassandra_version)
-
-
-def test_debian_apt_keyring_file(host):
-    if host.system_info.distribution == "debian" \
-            or host.system_info.distribution == "ubuntu":
-        f = host.file(get_cassandra_apt_keyring_path(host))
-
-        assert f.exists
+        assert "URIs: https://debian.cassandra.apache.org" in f.content_string
+        assert "Suites: {0}".format(cassandra_version) in f.content_string
+        assert "Signed-By: {0}".format(keyring_path) in f.content_string
 
 
 def test_debian_apt_search(host):
-    if host.system_info.distribution == "debian" \
-            or host.system_info.distribution == "ubuntu":
-        cmd = host.run("apt search cassandra")
+    if is_debian(host):
+        cmd = host.run("apt-cache policy cassandra")
 
         assert cmd.rc == 0
-        assert "cassandra" in cmd.stdout
+        assert "debian.cassandra.apache.org" in cmd.stdout
